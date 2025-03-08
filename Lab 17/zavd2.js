d733422fecd18d8306ca601b0a9f69bd1af9b1c5
Mykhailo Kuzmin <mishatapoek@gmail.com>
2025-03-08 18:33:37 +0200

    Lab 17

---
+++ Lab 17/zavd2.js
@@ -0,0 +1,12 @@
+function printEvenNumbers() {
+    let i = 0;
+
+    do {
+        if (i % 2 === 0) {
+            console.log(i);
+        }
+        i++;
+    } while (i <= 100);
+}
+
+printEvenNumbers();
